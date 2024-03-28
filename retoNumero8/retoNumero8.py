61f36b392f5a6534b51c1bb879853b1e94c6d14a
Neyen Frandino <neyenfrandino1@gmail.com>
2024-03-28 17:20:44 -0500

    Primera de 3 funciones lista

---
+++ retoNumero8/retoNumero8.py
@@ -0,0 +1,45 @@
+
+##Crea 3 funciones, cada una encargada de detectar si una cadena de
+##texto es un heterograma, un isograma o un pangrama.
+## Debes buscar la definición de cada uno de estos términos.
+
+
+##Heterograma:(Un heterograma es una palabra que no tiene ninguna letra repetida.)
+# "Jovencillo emponzoñado de whisky, qué mala figurota exhibes."
+
+##Isograma: (Esta palabra no tiene letras repetidas) = "Paralelepípedo"
+
+##Pangrama: (Esta frase incluye todas las letras del alfabeto español al menos una vez)
+## = "El veloz murciélago hindú comía feliz cardillo y kiwi."
+
+texto = 'Adulterinos q'
+def heterograma (text) :
+    acount_letter = ''
+    join_text = text.replace(" ", "")
+    for i in (range(ord('a'), ord('z') + 1)):
+        if join_text.count(chr(i)) == 1:
+            acount_letter += chr(i)
+
+    if len(acount_letter) == len(join_text):
+        print(f'Soy un Heterograma "{text.upper()}" ')
+
+    else:
+        print(f'No soy un Heterograma: {text}')
+        return
+
+
+
+
+
+    ##if abcdario == text :
+      ##  print(f'si soy un Heterograma: {text} ')
+
+
+
+
+
+
+
+
+
+heterograma(texto.lower())
